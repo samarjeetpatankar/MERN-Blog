472b718a207e20f33c7216a9f8270a0ee6f2cce8
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-12-06 14:38:46 +0530

    blog-app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Home from './Components/Home'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Home />
+      <Footer />
     </div>
   )
 }
--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import Hero from "./Hero";
-import Navbar from "./Navbar";
 import CTA from "./Cta";
-import Footer from "./Footer";
 
 const Home = () => {
   return (
     <div>
-      <Navbar />
       <Hero />
       <CTA />
-      <Footer />
     </div>
   );
 };
